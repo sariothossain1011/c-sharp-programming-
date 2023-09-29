dbb7c5236460fb8b35e8c818c8edb261ebf4bca6
Sariot Hossain <106438276+sariothossain1011@users.noreply.github.com>
2023-09-30 00:43:20 +0600

    study in decision making statement

---
+++ decision-making-statement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System ;
+
+namespace decisionMakingStatement{
+
+    class Program{
+        static void Main(string[]args){
+
+            // IF-ELSE
+            int a = 50;
+            int b = 40 ;
+            if(a > b){
+                Console.WriteLine("a greater then equal b");
+            }else{
+                Console.WriteLine("a less then equal b");
+            }
+        }
+    }
+
+}
---
+++ eventoaddcheck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace EvenToOddCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Enter Your Number:");
+            // int num = Convert.ToInt32(Console.ReadLine());
+
+            // if (type == typeof(num))
+            // {
+            //     if (num % 2 == 0)
+            //     {
+            //         Console.WriteLine("{0} Is even number.", num);
+            //     }
+            //     else if (num % 2 == 1)
+            //     {
+            //         Console.WriteLine("{0} Is odd number.", num);
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("This is not a Number! {0}", num);
+            //     };
+            // }
+            // else
+            // {
+            //     Console.WriteLine("This is not a number");
+            // }
+            // Console.ReadKey();
+        }
+    }
+}
